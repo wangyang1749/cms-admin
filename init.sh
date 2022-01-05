67fb48703df27ea751b512fed06f12580390f8b8
WangYang <1749748955@qq.com>
2022-01-05 20:49:55 +0800

    update

---
+++ init.sh
@@ -0,0 +1,20 @@
+CMS_PATH=~/cms/html/admin
+
+# echo $CMS_PATH
+# npm run build
+
+if [ ! -d $CMS_PATH ];then
+    mkdir -p $CMS_PATH
+    echo "创建：$CMS_PATH"
+else
+    echo "删除：$CMS_PATH"
+    rm -rf ${CMS_PATH}/*
+fi
+
+if [ "$(ls -A dist)" ];then
+    mv dist/* $CMS_PATH
+    echo "移动到：$CMS_PATH"
+else
+    echo "dist为空"
+fi
+
