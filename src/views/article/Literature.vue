aa0014ccbdd81b071038d84d981f00e178f7ae7e
wangyang <1749748955@qq.com>
2023-09-23 14:55:58 +0800

    update

--- src/views/article/Literature.vue
+++ src/views/article/Literature.vue
@@ -3,12 +3,7 @@
     <a-button @click="importData">导入</a-button>
     <a-button @click="generateHtml">HTML</a-button>
     <a-button @click="addMenu">添加菜单</a-button>
-    <a-button
-      type="primary"
-      icon="cloud-upload"
-      @click="() => (uploadVisible = true)"
-      >上传</a-button
-    >
+    <a-button type="primary" icon="cloud-upload" @click="() => (uploadVisible = true)">上传</a-button>
 
     <!-- <div v-for="item in literatures" :key="item.id">
       {{ item.id }}---{{ item.title }}--{{ item.url }}--{{ item.key }}
@@ -16,16 +11,30 @@
       <a href="javascript:;" @click="edit(item)">更新</a>
     </div> -->
 
- <a-table :pagination="false" :columns="columns" :dataSource="literatures" :rowKey="data => data.id">
+    <a-table :pagination="false" :columns="columns" :dataSource="literatures" :rowKey="data => data.id">
       <!-- <div slot="existNav" slot-scope="existNav,record">
         <a-switch defaultChecked @change="onChangeNav(record.id)" v-model="record.existNav" />
       </div> -->
-     <div slot="title_" slot-scope="title_">
+      <!-- <div slot="title_" slot-scope="title_">
         <a href="javascript:;">{{ title_ }}</a>
+      </div> -->
+      <div slot="title_" slot-scope="title_, record">
+        <a href="javascript:;" @click="preview(record.id)">{{ title_ }}</a>
+      </div>
+
+      <div slot="viewName" slot-scope="viewName, record">
+        <a href="javascript:;" @click="openHtml(record)">{{ viewName }}</a>
+      </div>
+      <div slot="categoryId" slot-scope="categoryId, record">
+        <a-select style="width: 100%" v-model="record.categoryId" @change="selectCategory(record.id, $event)">
+          <a-select-option :value="item.id" v-for="item in categorys" :key="item.id">{{ item.name }}</a-select-option>
+        </a-select>
       </div>
 
       <span slot="action" slot-scope="text, record">
-         <a href="javascript:;" @click="edit(record)">更新</a>
+        <a href="javascript:;" @click="generateHtml(record.id)">生成HTML</a>
+        <a-divider type="vertical" />
+        <a href="javascript:;" @click="edit(record)">更新</a>
         <a-divider type="vertical" />
         <a href="javascript:;" @click="deleteLiteratures(record)">删除</a>
         <a-divider type="vertical" />
@@ -37,17 +46,10 @@
       </span>
 
       <template slot="footer">
-        <div class="page-wrapper" :style="{ textAlign: 'right'}">
-          <a-pagination
-            class="pagination"
-            :current="pagination.page"
-            :total="pagination.total"
-            :defaultPageSize="pagination.size"
-            :pageSizeOptions="['1', '2', '5', '10', '20', '50', '100']"
-            showSizeChanger
-            @showSizeChange="handlePaginationChange"
-            @change="handlePaginationChange"
-          />
+        <div class="page-wrapper" :style="{ textAlign: 'right' }">
+          <a-pagination class="pagination" :current="pagination.page" :total="pagination.total"
+            :defaultPageSize="pagination.size" :pageSizeOptions="['1', '2', '5', '10', '20', '50', '100']" showSizeChanger
+            @showSizeChange="handlePaginationChange" @change="handlePaginationChange" />
         </div>
       </template>
     </a-table>
@@ -55,20 +57,9 @@
 
 
 
-    <a-modal
-      title="上传文献"
-      v-model="uploadVisible"
-      :afterClose="onUploadClose"
-      destroyOnClose
-    >
-      <a-upload-dragger
-        name="file"
-        :multiple="true"
-        :action="upload"
-        @change="uploadPic"
-        :headers="headers"
-        :withCredentials="true"
-      >
+    <a-modal title="上传文献" v-model="uploadVisible" :afterClose="onUploadClose" destroyOnClose>
+      <a-upload-dragger name="file" :multiple="true" :action="upload" @change="uploadPic" :headers="headers"
+        :withCredentials="true">
         <p class="ant-upload-drag-icon">
           <!-- <a-icon type="inbox" /> -->
           <img :src="literature.picPath" width="100%" alt srcset />
@@ -91,6 +82,9 @@
         <a-form-item label="key">
           <a-input v-model="literature.key"></a-input>
         </a-form-item>
+        <a-form-item label="originalContent">
+          <a-textarea v-model="literature.originalContent"></a-textarea>
+        </a-form-item>
         <!-- <a-form-item label="菜单的路径">
           <a-input v-model="literature.urlName"></a-input>
         </a-form-item> -->
@@ -103,11 +97,24 @@
 import literatureApi from "@/api/literature.js";
 import attachmentApi from "@/api/attachment.js";
 import preview from "@/api/preview.js";
+import categoryApi from "@/api/category.js";
+import contentAPI from "@/api/content.js";
 const columns = [
-     { title: "id", dataIndex: "id", key: "id" },
+  { title: "id", dataIndex: "id", key: "id" },
 
-   { title: "Key", dataIndex: "key", key: "key" },
-  { title: "Title", dataIndex: "title", key: "title",scopedSlots: { customRender: "title_" }},
+  { title: "Key", dataIndex: "key", key: "key" },
+  {
+    title: "分类",
+    dataIndex: "categoryId",
+    key: "categoryId",
+    scopedSlots: { customRender: "categoryId" },
+  },
+  { title: "Title", dataIndex: "title", key: "title", scopedSlots: { customRender: "title_" } }, {
+    title: "视图名称",
+    dataIndex: "viewName",
+    key: "viewName",
+    scopedSlots: { customRender: "viewName" },
+  },
   {
     title: "Action",
     key: "action",
@@ -127,7 +134,7 @@ export default {
         size: 5,
         sort: null
       },
-      literature:{},
+      literature: {},
       queryParam: {
         title: "",
         url: "",
@@ -137,10 +144,12 @@ export default {
         target: "_blank",
       },
       uploadVisible: false,
+      categorys: [],
     };
   },
   created() {
     this.loadLiterature();
+    this.loadcategory()
   },
   computed: {
     upload() {
@@ -164,7 +173,27 @@ export default {
         this.pagination.total = response.data.data.totalElements;
         // console.log(response);
       });
-    },handlePaginationChange(page, pageSize) {
+    }, loadcategory() {
+      // console.log("loadcategory");
+      categoryApi.list().then((response) => {
+        // console.log(response);
+        this.categorys = response.data.data;
+      });
+    }, selectCategory(value, select) {
+      contentAPI.updateCategory(value, select).then((response) => {
+        // console.log(response);
+        this.$notification["success"]({
+          message: "操作" + response.data.message,
+        });
+        this.loadLiterature();
+      });
+    }, generateHtml(id) {
+      contentAPI.generateHtml(id).then((response) => {
+        this.$notification["success"]({
+          message: "成功生成" + response.data.data.title + "的HTML",
+        });
+      });
+    }, handlePaginationChange(page, pageSize) {
       // console.log("111")
       // this.$log.debug(`Current: ${page}, PageSize: ${pageSize}`)
       this.pagination.page = page;
@@ -189,7 +218,7 @@ export default {
       } else if (status === "error") {
         this.$message.error(`${info.file.name} file upload failed.`);
       }
-    },onUploadClose(){
+    }, onUploadClose() {
 
     },
     edit(value) {
@@ -209,21 +238,24 @@ export default {
         });
         this.loadLiterature();
       });
-    },importData(){
-      literatureApi.import().then(resp=>{
+    }, importData() {
+      literatureApi.import().then(resp => {
         // console.log(resp)
         this.$message.success(resp.data.message);
         this.loadLiterature()
       })
-    },generateHtml(){
-      literatureApi.generateHtml().then(resp=>{
-        // console.log(resp)
-        this.$message.success(resp.data.message);
-        this.loadLiterature()
-      })
-    }, preview(id) {
-      window.open(preview.Online("literatureList", id), "_blank");
+    },
+
+    preview(id) {
+      window.open(preview.Online("content", id), "_blank");
       // window.location.href='https://www.baidu.com/'
+    },    openHtml(value) {
+      window.open(preview.Html(value.path + "/" + value.viewName), "_blank");
+      // if (value.haveHtml) {
+
+      // } else {
+      //   this.$message.error("该文章没有生成HTML");
+      // }
     },
     // updateMenu(value) {
     //   // console.log(value);
@@ -261,5 +293,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style></style>
